9e38420e24ccb8e7a2878793df0cc96e057d6083
Loc Tran <loc.tran@sstechvn.com>
2020-09-15 15:46:19 +0700

    Installing External Packages & Formatting Dates

--- demo_flutter_app/lib/main.dart
+++ demo_flutter_app/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 import './transaction.dart';
 
@@ -79,7 +80,8 @@ class MyHomePage extends StatelessWidget {
                         ),
                       ),
                       Text(
-                        tx.date.toString(),
+                        // DateFormat('yyyy-MM-dd').format(tx.date),
+                        DateFormat.yMMMd().format(tx.date),
                         style: TextStyle(color: Colors.grey),
                       ),
                     ],
